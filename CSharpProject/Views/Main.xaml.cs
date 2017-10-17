5aa81db62cb107a1728ad1279d5895d68d711365
EllenorMoshel <ellenor.moshel@hotmail.com>
2017-10-17 14:03:36 +0200

    Initial commit

---
+++ CSharpProject/Views/Main.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+
+
+namespace CSharpProject.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Logic/Entities/FeedItem.cs
@@ -0,0 +1,11 @@
+using System;
+using System.IO;
+
+namespace Logic.Entities
+{
+    public class FeedItem
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+    }
+}
